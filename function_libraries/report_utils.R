30d28dc5bb5c150942b2cf3beb9a09c6d0c7eb2f
Melissa Loudon <melissa.loudon@gmail.com>
2014-07-25 11:59:27 -0400

    added report util functions for split-by and monthly aggregate table loading
    added try-catch to monthly usage table generation to skip domains with errors

---
+++ function_libraries/report_utils.R
@@ -0,0 +1,35 @@
+# Utility functions for report modules
+
+# FUNCTION merged_monthly_table
+# gets all monthly.csv files for domains specified in domain_names
+# returns one merged table with all columns present in any monthly.csv
+#
+# PARAMS 
+# domain_names: a list of domain names to import monthly.csv files for
+# aggregate_tables_dir: the directory for aggregate tables,  should be output_dir/aggregate_tables
+merged_monthly_table <- function (domain_names, aggregate_tables_dir) {
+  library(plyr)
+  monthly_path <- function (dname){ return (file.path (aggregate_tables_dir,dname,"monthly.csv", fsep = .Platform$file.sep))}
+  filenames <- sapply(domain_names,monthly_path)
+  merged <- rbind.fill(lapply(filenames, read.csv,header = TRUE, as.is = TRUE))
+  return(merged)
+}
+
+# FUNCTION add_splitby_col
+# pulls the values of splitby_var from the domain table
+# adds then as a factor a column named split-by to the data table and returns it
+#
+# PARAMS 
+# data_table : the data to add a split_by column to
+# domain_table: the domain table
+# splitby_var: the variable to split by
+add_splitby_col <- function (data_table, domain_table, splitby_var) {
+  if (!(splitby_var %in% names(domain_table))) {
+    stop(sprintf ("Domain table has no attribute named %s", splitby_var))
+  }
+  splityby_frame <- subset(domain_table, select=c("name",splitby_var))
+  df <- merge(data_table,splityby_frame,by.x="domain",by.y="name")
+  names(df)[names(df) == splitby_var] <- 'split_by'
+  df$split_by <- sapply(df$split_by, as.factor)
+  return(df)
+}
