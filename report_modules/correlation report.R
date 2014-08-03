454e4b9a636d5e09f18643ce60516f2e0e7c44d6
oddmilk <ch.mengji@gmail.com>
2014-08-03 11:38:15 -0400

    fixed output directory of correlation report

--- report_modules/correlation report.R
+++ report_modules/correlation report.R
@@ -1,46 +1,38 @@
-# numeric indicators to be included in correlation report
-cont_vars <- c("visits", "nforms_per_month", "numeric_index", "days_on_cc", "total_case_touched", "median_visit_duration", 
-          "median_visits_per_active_day", "active_day_percent") # continuous variables
-cont_vars <- monthly_table[, cont_vars]
-
-
-# Correlation matrix for continuous variables 
-  # source code: http://ohiodata.blogspot.com/2012/06/correlation-tables-in-r-flagged-with.html
-corstarsl <- function(x){ 
-  require(Hmisc) 
-  x <- as.matrix(x) 
-  R <- rcorr(x)$r 
-  p <- rcorr(x)$P 
-  ## define notions for significance levels; spacing is important.
-  mystars <- ifelse(p < .001, "***", ifelse(p < .01, "** ", ifelse(p < .05, "* ", " ")))
-  ## trunctuate the matrix that holds the correlations to two decimal
-  R <- format(round(cbind(rep(-1.11, ncol(x)), R), 2))[,-1] 
-  ## build a new matrix that includes the correlations with their apropriate stars 
-  Rnew <- matrix(paste(R, mystars, sep=""), ncol=ncol(x)) 
-  diag(Rnew) <- paste(diag(R), " ", sep="") 
-  rownames(Rnew) <- colnames(x) 
-  colnames(Rnew) <- paste(colnames(x), "", sep="") 
-  ## remove upper triangle
-  Rnew <- as.matrix(Rnew)
-  Rnew[upper.tri(Rnew, diag = TRUE)] <- ""
-  Rnew <- as.data.frame(Rnew) 
-  ## remove last column and return the matrix (which is now a data frame)
-  Rnew <- cbind(Rnew[1:length(Rnew)-1])
-  return(Rnew) 
+library(Hmisc)
+# import and merge monthly usage data per domain (at this stage we are not doing domain specific correlations)
+render_debug <- function (test_data_dir, domains_for_run, report_options, output_dir) {
+  source(file.path("function_libraries","csv_sources.R", fsep = .Platform$file.sep))
+  domain_table <- get_domain_table_from_csv (test_data_dir)
+  create_monthly_usage(domain_table, domains_for_run, report_options, output_dir)
 }
 
-Rnew <- corstarsl(cont_vars) # this returns correlation matrix table with significance level 
-print(Rnew) 
-write.csv(Rnew, "corr_matrix.csv")
+render <- function (con, domains_for_run, report_options, output_dir) {
+  source(file.path("function_libraries","db_queries.R", fsep = .Platform$file.sep))
+  domain_table <- get_domain_table(con)
+  create_monthly_usage(domain_table, domains_for_run, report_options, output_dir)
+}
 
+create_monthly_usage <- function (domain_table, domains_for_run, report_options, output_dir) {
+  output_directory <- output_dir
+  read_directory <- file.path(output_directory,"aggregate_tables", fsep=.Platform$file.sep)
+  source(file.path("function_libraries","report_utils.R", fsep = .Platform$file.sep))
+  source(file.path("aggregate_tables","monthly_func.R", fsep = .Platform$file.sep))
+  all_monthly <- merged_monthly_table (domains_for_run, read_directory)
+  
+  #Run beginning of monthly usage report
+  all_monthly$batch_entry_percent= (all_monthly$batch_entry_percent)*100
+  all_monthly$median_visit_duration = round(all_monthly$median_visit_duration/60,
+                                            
+  
+  # numeric indicators to be included in correlation report 
+  cont_vars <- c("visits", "nforms_per_month", "numeric_index", "days_on_cc", "total_case_touched", "median_visit_duration", 
+                 "median_visits_per_active_day", "active_day_percent") # continuous variables
+  cont_vars <- monthly_table[, cont_vars]
+  
+  
+  # Correlation matrix for continuous variables (this function lives in report_utils.R)
+  Rnew <- corstarsl(cont_vars) # this returns correlation matrix table with significance level 
+  print(Rnew)   
+  write.csv(Rnew, file.path(output_dir, "correlation table.csv", fsep = .Platform$file.sep))
 
-
-
-
-
-
-
-
-
-
-
+}
