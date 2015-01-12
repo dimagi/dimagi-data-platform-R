520d1154ded1301abb7ef5a0614400b8ae66a78f
Rashmi <rdayalu@dimagi.com>
2015-01-12 03:47:11 +0000

    Updated aggregate_tables.json to include batch indicators in monthly section as well as lifetime section (previously forgot to do this).

--- analysis_scripts/rdayalu/mbti_jung.R
+++ analysis_scripts/rdayalu/mbti_jung.R
@@ -6,13 +6,20 @@
 mbti <- read.csv(file="mbti_results.csv")
 #Import file with hire date for each employee
 staff <- read.csv(file="staff_data.csv")
-staff <- select(staff, email, hire_date)
+staff <- select(staff, name, email, hire_date)
+
+#Manually change wrong email addresses
+mbti$email[mbti$email == "Mcanty@dimagi.com"] <- "mcanty@dimagi.com"
+mbti$email[mbti$email == "kumar@dimagi.com"] <- "vkumar@dimagi.com"
+
+staff$email[staff$email == "ashekar@gmail.com"] <- "ashekar@dimagi.com"
+staff$email[staff$email == "bbuczyk@dimagi.com"] <- "biyeun@dimagi.com"
+staff$email[staff$email == "ssynder@dimagi.com"] <- "ssnyder@dimagi.com"
 
 #Merge survey results with hire date
 mbti <- merge(mbti, staff, by.x = "email", by.y = "email", all.x = T)
 
 #Convert hire_date to date variable
-#Manually replace NA hire_dates with correct hire dates
 mbti$hire_date <- as.Date(mbti$hire_date, "%m/%d/%Y")
 mbti$hire_date[mbti$email == "Mcanty@dimagi.com"] <- "2014-10-01"
 mbti$hire_date[mbti$email == "ashekar@dimagi.com"] <- "2013-04-03"
@@ -23,4 +30,21 @@ mbti$hire_date[mbti$email == "ssnyder@dimagi.com"] <- "2012-03-26"
 #Manually replace BUs for people who reported > 1 BU. Use INC as default
 mbti$bu[mbti$bu == "DWA, INC"] <- "INC"
 mbti$bu[mbti$bu == "DLAC, INC"] <- "INC"
+
+#Code MBTI results to NBI
+mbti$nbi[mbti$mbti %in% c("INTJ", "ESTP", "ESFP", "INFJ")] <- "L1"
+mbti$nbi[mbti$mbti %in% c("ISTJ", "ISFJ", "ESTJ", "ENTJ")] <- "L2"
+mbti$nbi[mbti$mbti %in% c("INTP", "ENTP", "ENFP", "ISTP")] <- "R1"
+mbti$nbi[mbti$mbti %in% c("ISFP", "INFP", "ENFJ", "ESFJ")] <- "R2"
+
+#INC Summit workshop numbers
+#Variable "name" is the list of 56 people who are attending the INC summit
+#As of 1/11/15, we have 50 people who took the survey
+inc <- filter(mbti, !(name == ""))
+inc <- filter(test, !is.na(name))
+table(inc$nbi, useNA = "always")
+test <- filter(inc, nbi == "L2")
+test <- filter(inc, team == "Ops")
+
+
 
