242a3a47b7f14c87fca33ce9772014e083d40d75
Melissa Loudon <melissa.loudon@gmail.com>
2015-02-12 17:05:02 +0200

    separating out data prep from models, adding plots

--- analysis_scripts/melissa_dissertation.R
+++ analysis_scripts/melissa_dissertation.R
@@ -11,7 +11,16 @@ library(ggplot2)
 start_date <- as.Date("2010-01-01")
 end_date <- as.Date("2014-12-31")
 
-add_domain_month_index<-function(monthly_table){
+# probabilities from log-odds
+prob <- function(x) {return (exp(x)/(1+exp(x)))}
+
+# geometric mean
+gm_mean = function(x, na.rm=TRUE){
+  exp(sum(log(x[x > 0]), na.rm=na.rm) / length(x))
+}
+
+# add an index for how many months the domain has been on CC
+add_domain_month_index<-function(monthly_table) {
   domain_start_dates <- monthly_table %>% group_by (domain) %>% summarise(domain_start_date = min(month_start) )
   monthly_table <- merge(monthly_table,domain_start_dates,by=c("domain"))
   
@@ -31,9 +40,7 @@ add_domain_month_index<-function(monthly_table){
   domain_indexes <- monthly_table %>% group_by (domain, month.index) %>% do(domain_month_index = domain_month_index(.) )
   monthly_table <- merge(monthly_table,domain_indexes,by=c("domain","month.index"))
   monthly_table$domain_month_index<-monthly_table$domain_month_index[[1]]
-  return(monthly_table)
-  
-  
+  return(monthly_table) 
 }
 
 clean_monthly_table <- function(monthly_table,domain_table,user_table) {
@@ -73,95 +80,26 @@ clean_monthly_table <- function(monthly_table,domain_table,user_table) {
   return(monthly_table)
 }
 
-gm_mean = function(x, na.rm=TRUE){
-  exp(sum(log(x[x > 0]), na.rm=na.rm) / length(x))
-}
-
-# tests whether various domain attributes effect retention percentage
-# retention percentage is the number of users submitting data in month n who also submit data in month n+diff
-test_retention_predictors <- function(monthly_table, n_month, diff, end_date) {
-  # get month n data only
-  month.n.data <- monthly_table %>% filter(numeric_index == n_month)
-  # get which users active in month n were active in month n+diff
-  mdiff_active_users <- monthly_table %>% filter(numeric_index == n_month+diff) %>% select(domain,user_id)
-  dropouts <- anti_join(month.n.data,mdiff_active_users,by=c("domain","user_id")) %>% select(domain,user_id)
-  dropouts$active_diff <- F
-  month.n.data <- merge(month.n.data, dropouts, by=c("domain","user_id"), all.x=T, all.y=F)
-  month.n.data[is.na(month.n.data$active_diff),]$active_diff <- T
-  
-  # only domains with > 3 users in month n
-  month.n.domains <- month.n.data %>% group_by(domain) %>% summarise(total_users = length(unique(user_id))) %>%
-    filter (total_users > 3)
-  month.n.data <- month.n.data %>% filter(domain %in% month.n.domains$domain)
-  
-  month.n.by.domain <- month.n.data %>% group_by(domain) %>%
-    summarise(first_active_month_start = min(month_start),
-              total_users = length(unique(user_id)),
-              percent_retained = (sum(active_diff) / length(unique(user_id)))*100,
-              percent_android = (length(unique(user_id[summary_device_type=="Android"])) / length(unique(user_id)))*100,
-              most_common_device = names(sort(table(summary_device_type),decreasing=TRUE)[1]))
-  
-  month.n.by.domain <- merge(month.n.by.domain,self_starts,by="domain",all.x=T,all.y=F)
-  
-  # take out domains that are less than n+diff months old
-  cutoff <- as.Date(as.yearmon(end_date) -((n_month+diff-1)/12), frac = 0)
-  month.n.by.domain <- month.n.by.domain %>% 
-    filter(first_active_month_start<=cutoff)
-  table(month.n.by.domain$most_common_device, month.n.by.domain$self_started,dnn=c("device","self-started"))
-  
-  month.n.by.domain$most_common_device <- as.factor(month.n.by.domain$most_common_device)
-  print(kruskal.test(percent_retained~most_common_device,data=month.n.by.domain))
-  most_common_device.summary <- month.n.by.domain %>% group_by(most_common_device) %>% summarise(median(percent_retained), total_domains=n())
-  print(most_common_device.summary)
-  
-  month.n.by.domain$self_started <- as.factor(month.n.by.domain$self_started)
-  print(kruskal.test(percent_retained~self_started,data=month.n.by.domain))
-  self_started.summary<-month.n.by.domain %>% group_by(self_started) %>% summarise(median(percent_retained), total_domains=n())
-  print(self_started.summary)
-  
-  
-}
-
-get_visits_models <- function(monthly_table, n_month, exclude_names=list(),exclude_na = T){
+get_visits_data <- function(monthly_table, n_month, exclude_names=list(),exclude_na = T){
   # get month n data only
   month.n.data <- monthly_table %>% filter(numeric_index == n_month)%>% 
-    select(domain,nvisits,self_started,summary_device_type)
+    select(domain,user_id,nvisits,self_started,summary_device_type)
   
   # remove rows where any value is NA
   if (exclude_na==T) {month.n.data <- na.omit(month.n.data)}
-  
   # filter out domains on the exclude list
   month.n.data <- month.n.data %>% filter(!(domain %in% exclude_names))
   
-  ggplot(month.n.data ,aes(x=domain,y=nvisits,color=summary_device_type)) +
-    geom_point(size=2) 
-  ggplot(month.n.data ,aes(x=domain,y=nvisits,color=self_started)) +
-    geom_point(size=2) 
-  
-  # summary tables
-  month.n.by.domain <- month.n.data %>% group_by(domain, self_started) %>% summarise(total_users = length(unique(user_id)),
-                                                                                     median_forms = median(nforms),
-                                                                                     median_visits = median(nvisits),
-                                                                                     mean_visits = mean(nvisits),
-                                                                                     gm_mean_visits = gm_mean(nvisits),
-                                                                                     percent_android = (length(unique(user_id[summary_device_type=="Android"])) / length(unique(user_id)))*100,
-                                                                                     most_common_device = names(sort(table(summary_device_type),decreasing=TRUE)[1]))
-  
-  #ggplot(month.n.by.domain,aes(x=domain,y=mean_visits,color=self_started,shape=most_common_device)) +
-    geom_point(size=5) 
-  
-  month.n.by.domain$self_started <- as.factor(month.n.by.domain$self_started)
-  month.n.by.domain$most_common_device <- as.factor(month.n.by.domain$most_common_device)
-  most_common_device.summary <- month.n.by.domain %>% group_by(most_common_device) %>% 
-    summarise(median(median_visits), median(median_forms), total_domains=n())
-  self_started.summary <- month.n.by.domain %>% group_by(self_started) %>% 
-    summarise(median(median_visits), median(median_forms), total_domains=n())
-  print(self_started.summary)
-  print(most_common_device.summary)
-  
-  # models for nvisits
+  # log transform
   month.n.data$log_visits = log(month.n.data$nvisits)
   
+  return(month.n.data)
+}
+
+get_visits_models <- function(visits_data) {
+  
+  month.n.data <- visits_data
+
   m.visits.null <- lmer(log_visits ~ 1+ (1|domain), data = month.n.data, REML=T)
   m.visits.self_started <- lmer(log_visits ~ self_started + (1|domain), data = month.n.data, REML=T)
   m.visits.device <- lmer(log_visits ~ summary_device_type + (1|domain), data = month.n.data,
@@ -169,18 +107,16 @@ get_visits_models <- function(monthly_table, n_month, exclude_names=list(),exclu
   m.visits.full <- lmer(log_visits ~ summary_device_type + self_started+ (1|domain), data = month.n.data,
                         REML=T)
   
-  drop1( m.visits.full)
-  
   ret <- c(m.visits.full, m.visits.self_started,m.visits.device,m.visits.null)
   names(ret) <- c("full","no_device","no_self_started","null")
   return (ret)
   
 }
 
-get_retention_models <- function(monthly_table, n_month, diff, exclude_names = list(),exclude_na = T){
+get_retention_data <- function(monthly_table, n_month, diff, exclude_names = list(),exclude_na = T){
   # get month n data only
   month.n.data <- monthly_table %>% filter(numeric_index == n_month) %>% 
-  select(domain,user_id,month.index, month_start,self_started,summary_device_type)
+    select(domain,user_id,month.index, month_start,self_started,summary_device_type)
   
   # remove rows where any value is NA
   if (exclude_na==T) {month.n.data <- na.omit(month.n.data)}
@@ -199,6 +135,13 @@ get_retention_models <- function(monthly_table, n_month, diff, exclude_names = l
     select(user_id,domain,first_month_start)    
   month.n.data <- merge(month.n.data, user_start_dates,by=c("domain","user_id"),all.x=T,all.y=F)
   month.n.data <- month.n.data %>% filter(first_month_start <= cutoff)
+  
+  return(month.n.data)
+}
+
+get_retention_models <- function(retention_data){
+  
+  month.n.data <- retention_data
   
   # models for retention
   m.retention.null <- glmer(active_diff ~ 1+ (1|domain), data = month.n.data, family=binomial(link=logit),
@@ -241,16 +184,17 @@ user_table <- read.csv("user_table.csv")
 
 monthly_table <- clean_monthly_table(monthly_table,domain_table,user_table)
 
-
 # visits models for month 6
-m6.models <- get_visits_models(monthly_table,6)
+m6.data <- get_visits_data(monthly_table,6)
+m6.models <- get_visits_models(m6.data)
 
 anova(m6.models$full,m6.models$no_device)
 anova(m6.models$full,m6.models$no_self_started)
 
 influential_domains(m6.models$no_self_started,parameters=c(2))
 
-m6.models.rev <- get_visits_models(monthly_table,6,c("crs-mip","m4change","project"))
+m6.data.rev <- get_visits_data(monthly_table,6,c("crs-mip","m4change","project"))
+m6.models.rev <- get_visits_models(m6.data.rev)
 anova(m6.models.rev$full,m6.models.rev$no_device)
 anova(m6.models.rev$no_self_started,m6.models.rev$full)
 
@@ -262,7 +206,8 @@ exp(confint(m6.models.rev$no_self_started, level = 0.95, method="profile"))
 r.squaredGLMM(m6.models.rev$no_self_started)
 
 # visits models for month 12
-m12.models <- get_visits_models(monthly_table,12)
+m12.data <- get_visits_data(monthly_table,12)
+m12.models <- get_visits_models(m12.data)
 
 anova(m12.models$full,m12.models$no_device)
 anova(m12.models$full,m12.models$no_self_started)
@@ -275,8 +220,8 @@ exp(confint(m12.models.rev$no_self_started, level = 0.95, method="profile"))
 r.squaredGLMM(m12.models$no_self_started)
 
 # visits models for month 18
-
-m18.models <- get_visits_models(monthly_table,18)
+m18.data <- get_visits_data(monthly_table,18)
+m18.models <- get_visits_models(m18.data)
 
 anova(m18.models$full,m18.models$no_device)
 anova(m18.models$full,m18.models$no_self_started)
@@ -287,32 +232,108 @@ exp(fixef(m18.models$no_self_started))
 exp(confint(m18.models$no_self_started, level = 0.95, method="profile"))
 r.squaredGLMM(m18.models$no_self_started)
 
-
-# retention for month 6 (users still active at month 9)
-prob <- function(x) {return (exp(x)/(1+exp(x)))}
-m6.ret.models <- get_retention_models(monthly_table,6,3)
+# retention for month 6 (users still active at month 12)
+m6.ret.data <- get_retention_data(monthly_table,6,6)
+m6.ret.models <- get_retention_models(m6.ret.data)
 anova(m6.ret.models$full,m6.ret.models$no_device)
 anova(m6.ret.models$full,m6.ret.models$no_self_started)
 influential_domains(m6.ret.models$no_device, parameters=c(2))
 
 summary(m6.ret.models$no_device)
+exp(confint(m6.ret.models$no_device, level = 0.95, method="boot"))
 r.squaredGLMM(m6.ret.models$no_device)
 
 # retention for month 2 (users still active at month 5)
-m2.ret.models <- get_retention_models(monthly_table,2,3)
+m2.ret.data <- get_retention_data(monthly_table,2,3)
+m2.ret.models <- get_retention_models(m2.ret.data)
 anova(m2.ret.models$full,m2.ret.models$no_device)
 anova(m2.ret.models$full,m2.ret.models$no_self_started)
-summary(m2.ret.models$no_self_started)
 influential_domains(m2.ret.models$no_self_started, parameters=c(2))
 
-# plots
-library(sjPlot)
-sjp.lmer(m6.models$no_self_started)
-sjp.glmer(m6.models$no_self_started, type = "re.qq")
+summary(m2.ret.models$no_self_started)
+exp(confint(m2.ret.models$no_self_started, level = 0.95, method="boot"))
+exp(fixef(m2.ret.models$no_self_started))
+r.squaredGLMM(m2.ret.models$no_self_started)
 
-sjp.glmer(m6.ret.models$no_device, type = "fe.pc")
-sjp.glmer(m6.ret.models$no_device,
-          type = "ri.pc",
-          facet.grid = FALSE)
+# plots
+cbPalette <- c("#56B4E9", "#D55E00")
+cbPalette2 <- c("#0072B2", "#F0E442")
+m2.ret.by.domain <- m2.ret.data %>% group_by(domain, self_started) %>% 
+  summarise(retention_percent = (sum(active_diff)/n()*100),
+            nusers = n(),
+            most_common_device = names(sort(table(summary_device_type),decreasing=TRUE)[1]))
+ggplot(m2.ret.by.domain,aes(x=domain,y=retention_percent,color=most_common_device)) +
+  geom_point(aes(size=nusers)) + labs(x = "Project", y = "Percent of users retained", 
+                                      size="Total users", color = "Device type") +
+  scale_size(trans="log10") +
+  scale_colour_manual(values=cbPalette) +
+  theme(axis.text.x = element_blank())
+
+m6.ret.by.domain <- m6.ret.data %>% group_by(domain, self_started) %>% 
+  summarise(retention_percent = (sum(active_diff)/n()*100),
+            nusers = n(),
+            most_common_device = names(sort(table(summary_device_type),decreasing=TRUE)[1]))
+ggplot(m6.ret.by.domain,aes(x=domain,y=retention_percent,color=self_started)) +
+  geom_point(aes(size=nusers)) + labs(x = "Project", y = "Percent of users retained", 
+                                      size="Total users", color = "Self-started?") +
+  scale_size(trans="log10") +
+  scale_colour_manual(values=cbPalette2) +
+  theme(axis.text.x = element_blank())
+
+ggplot(m6.data,aes(x=domain,y=nvisits,color=summary_device_type)) +
+  geom_point() +
+  labs(x = "Project", y = "Number of visits", color="Device type") +
+  scale_colour_manual(values=cbPalette) +
+  theme(axis.text.x = element_blank())
+
+m6.by.domain<- m6.data.rev %>% group_by(domain, self_started) %>% 
+  summarise(total_users = length(unique(user_id)),
+            median_visits = median(nvisits),
+            mean_visits = mean(nvisits),
+            gm_mean_visits = gm_mean(nvisits),
+            percent_android = (length(unique(user_id[summary_device_type=="Android"])) / length(unique(user_id)))*100,
+            most_common_device = names(sort(table(summary_device_type),decreasing=TRUE)[1]))
+
+m6.plot<-ggplot(m6.by.domain,aes(x=domain,y=median_visits,color=most_common_device)) +
+  geom_point(aes(size=total_users)) + labs(x = "Project", y = "Median visits", 
+                                      size="Total users", color = "Device type") +
+  scale_size(trans="log10") +
+  scale_colour_manual(values=cbPalette2) +
+  theme(axis.text.x = element_blank())
+
+m12.by.domain<- m12.data %>% group_by(domain, self_started) %>% 
+  summarise(total_users = length(unique(user_id)),
+            median_visits = median(nvisits),
+            mean_visits = mean(nvisits),
+            gm_mean_visits = gm_mean(nvisits),
+            percent_android = (length(unique(user_id[summary_device_type=="Android"])) / length(unique(user_id)))*100,
+            most_common_device = names(sort(table(summary_device_type),decreasing=TRUE)[1]))
+
+m12.plot<-ggplot(m12.by.domain,aes(x=domain,y=median_visits,color=most_common_device)) +
+  geom_point(aes(size=total_users)) + labs(x = "Project", y = "Median visits", 
+                                           size="Total users", color = "Device type") +
+  scale_size(trans="log10") +
+  scale_colour_manual(values=cbPalette2) +
+  theme(axis.text.x = element_blank())
+
+m18.by.domain<- m18.data %>% group_by(domain, self_started) %>% 
+  summarise(total_users = length(unique(user_id)),
+            median_visits = median(nvisits),
+            mean_visits = mean(nvisits),
+            gm_mean_visits = gm_mean(nvisits),
+            percent_android = (length(unique(user_id[summary_device_type=="Android"])) / length(unique(user_id)))*100,
+            most_common_device = names(sort(table(summary_device_type),decreasing=TRUE)[1]))
+
+m18.plot<-ggplot(m18.by.domain,aes(x=domain,y=median_visits,color=most_common_device)) +
+  geom_point(aes(size=total_users)) + labs(x = "Project", y = "Median visits", 
+                                           size="Total users", color = "Device type") +
+  scale_size(trans="log10") +
+  scale_colour_manual(values=cbPalette2) +
+  theme(axis.text.x = element_blank())
+
+
+
+
+
 
 
