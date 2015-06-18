a5aa77ab59ddba74ef19264b7e4f6623dde1e935
Mengji Chen <mchen@ip-10-171-72-92.ec2.internal>
2015-06-18 15:09:33 +0000

    update visualization script for blog#6

--- analysis_scripts/mchen/blog_6/hq_viz.R
+++ analysis_scripts/mchen/blog_6/hq_viz.R
@@ -54,7 +54,57 @@ texp = left_join(texp, piles)
 texp$piled_blocks = texp$incre_blocks + texp$max_blocks # building blocks into mounds for each domain
 
 
-# subset HQ domains (just run codes above and you will get the data)
-# needs to be functioned
+# all HQ domains
+# geom_tile plot 
+suppressMessages(library(ggplot2))
+suppressMessages(library(scales))
+suppressMessages(library(RColorBrewer))
+
+x_axis_ticks = as.character(seq(min(texp$calendar_month),max(texp$calendar_month),by="quarter")) # R considers Date variable as continuous
+x_axis_labels = format(as.Date(x_axis_ticks), "%b %y")  
+
+g0 = ggplot(texp, aes(x=as.character(calendar_month),y=piled_blocks)) + 
+  geom_tile(aes(fill=factor(texp$b_rank))) + 
+  guides(fill=FALSE) + # turning off legends
+  scale_fill_manual(values=rep(c("#005266", "#882608", "#fe9c7b"),
+                               ceiling(n_distinct(texp$domain_numeric)/3))) + # repeating colors to distinguish each domain
+  scale_y_continuous(breaks = NULL) + # suppressing ticks, gridlines
+  scale_x_discrete(breaks = x_axis_ticks, labels = x_axis_labels) + # reduce total number of breaks
+  theme(axis.title.x = element_blank(), 
+        axis.title.y = element_blank(), # hide titles for both x- and y-axis
+        axis.text.x = element_text(face="bold",
+                                   colour="#004865", size=11),
+        panel.background = element_rect(fill="#d5e4eb"),
+        panel.grid = element_blank())  
+
+ggsave("hq_viz.pdf", g0, width = 8, height = 12)
+
+
+# subset HQ domains 
 # subs = c(11, 67, 87, 127, 138)
+# run line 17-29 and you will get the data
 t = filter(t, domain_numeric %in% subs)
+hq_sub = ggplot(t_agg_1, aes(x=calendar_month, y=nusers)) +
+  geom_bar(width=28,stat="identity") +
+  facet_wrap(~domain_numeric, nrow=5, scales = "free_y") + 
+  theme(axis.title.x = element_blank(), 
+        axis.title.y = element_blank(), # hide titles for both x- and y-axis
+        axis.text.x = element_text(face="bold",
+                                   colour="#004865", size=11),
+        panel.background = element_rect(fill="#d5e4eb"),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        panel.grid.minor.y = element_blank())
+
+# highlight time range
+rect = data.frame(xmin = as.Date(c("2012-04-01","2012-10-01","2013-04-01","2013-10-01","2014-04-01","2014-10-01")), 
+                  xmax = as.Date(c("2012-06-01","2012-12-01","2013-06-01","2013-12-01","2014-06-01","2014-10-01")),
+                  ymin = c(-Inf,-Inf,-Inf,-Inf,-Inf,-Inf),
+                  ymax = c(Inf,Inf,Inf,Inf,Inf,Inf))
+hq_sub_2 = hq_sub + geom_rect(data = rect, aes(xmin=xmin, xmax=xmax,
+                                      ymin = ymin, ymax=ymax),
+                     fill = "#aaaea6",
+                     alpha = 0.3,
+                     inherit.aes = FALSE) 
+
+ggsave("hq_subdomains.pdf", hq_sub_2, width = 8, height = 8)
